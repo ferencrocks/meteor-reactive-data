fbd422c2819c76602660779adb0df0228a1ec7f9
Ferenc Faluvegi <frantzdae@live.com>
2017-06-23 08:03:53 +0300

    Test app

--- client/main.js
+++ client/main.js
@@ -1,22 +1,64 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
 
-import './main.html';
+import ReactiveData, { Fields, DataView, DataItems, PrevNextPaginator } from 'meteor/ferencrocks:meteor-reactive-data';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const CustomPaginator = props => {
+  return null;
+};
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+const App = () => (
+  <ReactiveData
+    id="users"
+    fields={[
+      {key: 'firstName'},
+      {key: 'lastName'}
+    ]}
+    data={[
+      {firstName: 'Faluvegi', lastName: 'Ferenc'},
+      {firstName: 'Zsiros', lastName: 'Miska'},
+      {firstName: 'Faluvegi', lastName: 'Ferenc'},
+      {firstName: 'Zsiros', lastName: 'Miska'},
+      {firstName: 'Faluvegi', lastName: 'Ferenc'},
+      {firstName: 'Zsiros', lastName: 'Miska'},
+      {firstName: 'Faluvegi', lastName: 'Ferenc'},
+      {firstName: 'Zsiros', lastName: 'Miska'},
+      {firstName: 'Faluvegi', lastName: 'Ferenc'},
+      {firstName: 'Zsiros', lastName: 'Miska'},
+      {firstName: 'Faluvegi', lastName: 'Ferenc'},
+      {firstName: 'Zsiros', lastName: 'Miska'}
+    ]}
+    rowsPerPage={5}
+    dataView="table"
+  >
+    <DataView name="table">
+      <table className="table table-bordered">
+        <Fields
+          render={field => <th>{field.key}</th>}
+          wrapper={thead => (
+            <thead>
+            <tr>{thead}</tr>
+            </thead>
+          )}
+        />
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+        <DataItems
+          render={doc => (
+            <Fields
+              render={field => <td>{doc[field.key]}</td>}
+              wrapper={row => <tr>{row}</tr>}
+            />
+          )}
+          wrapper={content => <tbody>{content}</tbody>}
+        />
+      </table>
+
+      <PrevNextPaginator />
+    </DataView>
+  </ReactiveData>
+);
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('test-root'));
 });
